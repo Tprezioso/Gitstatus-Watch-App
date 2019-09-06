9de70803c9e3e8dee5ad762047493fce52d382a4
Tprezioso <tommyprezioso@gmail.com>
2019-09-06 19:57:40 -0400

    added some stuff and organized

---
+++ GitStatusWatchApp WatchKit Extension/Model/GitStatusModel.swift
@@ -0,0 +1,14 @@
+//
+//  GitStatusModel.swift
+//  GitStatusWatchApp WatchKit Extension
+//
+//  Created by Thomas Prezioso on 9/6/19.
+//  Copyright © 2019 Thomas Prezioso. All rights reserved.
+//
+
+import Foundation
+
+struct GitStatusObject {
+    var name: String
+    var status: String
+}
