d89ec8400b8c0cd4d09f83304dd2ba285cfe676e
Tprezioso <tommyprezioso@gmail.com>
2019-11-20 18:39:19 -0500

    formatted date for incidents

--- GitStatusWatchApp WatchKit Extension/View Model/IncidentViewModel.swift
+++ GitStatusWatchApp WatchKit Extension/View Model/IncidentViewModel.swift
@@ -11,29 +11,40 @@ import SwiftUI
 import Combine
 
 class IncidentViewModel: ObservableObject {
-  
+    
     @Published var incident = GitIncidentsObject(name: "", dateCreated: "", impact: "", body: "")
     @Published var array = [GitIncidentsObject]()
     
     var apiJSON = [[String : Any]]()
-   
-     func fetchStatus() {
+    
+    func fetchStatus() {
         APICALL().lastIncident { (json) in
             self.apiJSON = json! as [[String : Any]]
-//            for response in self.apiJSON {
             self.incident.name = self.apiJSON[0]["name"] as! String
-                self.incident.dateCreated = self.apiJSON[0]["created_at"] as! String
-                self.incident.impact = self.apiJSON[0]["impact"] as? String ?? "No impact"
+            self.incident.dateCreated = self.formatDate(date: self.apiJSON[0]["created_at"] as! String)
+            
+            self.incident.impact = self.apiJSON[0]["impact"] as? String ?? "No impact"
             if let detailArray = self.apiJSON[0]["incident_updates"] as? [[String:Any]] {
                 self.incident.body = detailArray[0]["body"] as! String
                 print(detailArray[0]["body"]!)
             }
-
-                self.array.append(self.incident)
-//            print(self.apiJSON[0]["incident_updates"]!)
-//            }
+            
+            self.array.append(self.incident)
         }
     }
     
+    func formatDate(date: String) -> String {
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        //    dateFormatter.locale = Locale(identifier: "en_US") //uncomment if you don't want to get the system default format.
+        
+        let dateObj: Date? = dateFormatterGet.date(from: date)
+        
+        return dateFormatter.string(from: dateObj!)
+    }
+    
 }
-
