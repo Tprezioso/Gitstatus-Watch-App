90ceeaef15ecaded772fcf0aa36ef0d0982822f6
Tprezioso <tommyprezioso@gmail.com>
2019-11-26 21:50:58 -0500

    tryoing to figure out progress hud

--- GitStatusWatchApp WatchKit Extension/View/ContentView.swift
+++ GitStatusWatchApp WatchKit Extension/View/ContentView.swift
@@ -7,30 +7,50 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView : View {
     
     @ObservedObject var statusList = StatusListViewModel()
     
+    @State var spinCircle = true
+    @State var hideLoading = false
+    
     var body: some View {
-        List {
-            ForEach(statusList.array) { status in
-                VStack(alignment: .leading) {
-                    Text("\(status.name):")
-                        .fontWeight(.heavy)
-                        .multilineTextAlignment(.leading)
-                    
-                    Spacer()
-                    Text(status.status)
+//        ZStack {
+            List {
+                ForEach(statusList.array) { status in
+                    VStack(alignment: .leading) {
+                        Text("\(status.name):")
+                            .fontWeight(.heavy)
+                            .multilineTextAlignment(.leading)
+                        
+                        Spacer()
+                        Text(status.status)
+                    }
+                    .padding(20)
                 }
-                .padding(20)
             }
-        }
-        .listStyle(CarouselListStyle())
-        .navigationBarTitle(Text("GitHub Status"))
-        .onAppear(perform: {
-            self.statusList.fetchStatus()
-        })
+            .listStyle(CarouselListStyle())
+            .navigationBarTitle(Text("GitHub Status"))
+            .onAppear(perform: {
+                self.statusList.fetchStatus()
+                self.spinCircle.toggle()
+
+            })
+
+//            Circle()
+//                .trim(from: 0.5, to: 1)
+//                .stroke(Color.blue, lineWidth:4)
+//                .frame(width:100)
+//                .rotationEffect(.degrees(spinCircle ? 0 : -360), anchor: .center)
+//                .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+//                .onAppear(perform: {
+//                    self.statusList.fetchStatus()
+//                    self.spinCircle.toggle()
+//
+//                })
+//        }
     }
 }
 
--- Podfile
+++ Podfile
@@ -12,7 +12,6 @@ target 'GitStatusWatchApp WatchKit App' do
   # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
   # use_frameworks!
   pod 'Alamofire'
-  pod 'Progress'
   # Pods for GitStatusWatchApp WatchKit App
 
 end
@@ -21,7 +20,6 @@ target 'GitStatusWatchApp WatchKit Extension' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
   pod 'Alamofire'
-  pod 'Progress'
   # Pods for GitStatusWatchApp WatchKit Extension
 
 end
